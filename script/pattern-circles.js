e2cdb6ff6adb84d80fc472399d1d6e61be2810bc
Matthew Falconer <msfalconer.dev@gmail.com>
2024-06-01 21:20:07 +0100

    rename example to circles

--- script/pattern-circles.js
+++ script/pattern-circles.js
@@ -2,9 +2,9 @@
 const squares = new Array(20);
 const iterations = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 squares.fill(true);
+
 // const colors = ['#F7F9F9', '#BED8D4', "#78D5D7", "#6ED4EB", "#63D2FF", "#42AAE1", "#2081C3"];
 const colors = ['#F7F9F9', "#6ED4EB", "#2081C3"];
-
 
 
 
@@ -12,23 +12,16 @@ function setup() {
     // width, height
     const width = windowWidth;
     const height = windowHeight;
-
-    const squareWidth = windowWidth / squares.length;
     const canvas = createCanvas(width, height);
+
     // will find element with id "first-canvas" and inject canvas
     canvas.parent('first-canvas');
-
-    background(200);
-
-    frameRate(20);
-
-
 
 }
 
 function draw() {
 
-    iterations.forEach((item, index) => {
+    iterations.forEach((_item, index) => {
         createSquares(index);
     });
 
@@ -54,7 +47,7 @@ function createSquares(y) {
         circle(index * squareWidth, (squareWidth / 2) + (squareWidth * y), squareWidth / 1);
 
         fill(colorCircleTwo); 
-        circle(index * squareWidth, (squareWidth / random([2, 3, 4])) + (squareWidth * y), squareWidth / (random([2, 4, 8])));
+        circle(index * squareWidth, (squareWidth / random([1.5, 2, 3.5])) + (squareWidth * y), squareWidth / (random([2, 4, 6])));
     })
 }
 
